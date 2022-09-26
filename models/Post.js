e17c128ab4132a9566a4d0f1bd6c35feb166241b
kieferjackson <kieferleejackson@gmail.com>
2022-09-26 13:29:50 -0700

    Created model for posts

---
+++ models/Post.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init
+(
+    {
+        id:
+        {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title:
+        {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        submit_date: 
+        {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        message:
+        {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        poster_id:
+        {
+            type: DataTypes.INTEGER,
+            references:
+            {
+                model: 'gallery',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
