49be7e93182ed6ca1f39a67b3ee961525fa412f2
kieferjackson <kieferleejackson@gmail.com>
2022-09-27 15:06:33 -0700

    Added routes for getting all posts and all users

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,42 @@
 const router = require('express').Router();
-const { User, Post } = require('../../models');
+const { User, Post } = require('../models');
 
 // GET all posts
+router.get('/', async (req, res) =>
+{
+    try
+    {
+        const allPosts = await Post.findAll();
+
+        const posts = allPosts.map( (post) => post.get({ plain: true }));
+
+        // Return all posts in the database
+        res.status(200).json(posts);
+    }
+    catch (error)
+    {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
+
+// GET all users
+router.get('/users', async (req, res) =>
+{
+    try
+    {
+        const allUsers = await User.findAll();
+
+        const users = allUsers.map( (user) => user.get({ plain: true }));
+
+        // Return all posts in the database
+        res.status(200).json(users);
+    }
+    catch (error)
+    {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
 
 module.exports = router;
