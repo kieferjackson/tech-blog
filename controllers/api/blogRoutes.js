c35eb80e7567cb7c9c5a5882cf0fd6613e09c0ee
kieferjackson <kieferleejackson@gmail.com>
2022-10-18 15:00:19 -0700

    POST route for adding posts + config changes

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -2,5 +2,33 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 
 // CREATE new post
+router.post('/', async (req, res) => {
+    try {
+        const { title, message, submit_date } = req.body;
+
+        if (req.session.loggedIn) {
+            const new_post = await Post.create
+                (
+                    {
+                        title,
+                        message,
+                        submit_date,
+                        poster_id: req.session.user_id,
+                    }
+                );
+
+            res.redirect(`/dashboard/${req.session.user_id}`);
+            return;
+        }
+        else {
+            res.status(400).json({ message: `Insufficient information to add pet` });
+        }
+
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
 
 module.exports = router;
